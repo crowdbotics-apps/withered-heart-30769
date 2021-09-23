e8d323bcde8bf5e8348df21413f31a1c256e1a86
Crowdbotics <team@crowdbotics.com>
2021-09-23 10:34:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "withered_heart_30769Identifier",
+  url: "https://withered_heart_30769Identifier.botics.co",
+  api: "https://withered_heart_30769Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
